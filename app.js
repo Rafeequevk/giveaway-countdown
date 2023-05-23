2a837dd22c77a8022c3247058aecf03f90ffb3ef
Rafeeque <rafeequekottakkal046@gmail.com>
2023-05-24 00:30:00 +0530

    added a feature for testing purpose

--- app.js
+++ app.js
@@ -27,7 +27,14 @@ console.log(giveAwayDate.textContent);
 const deadLine = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2023, 06, 24, 11, 30, 00, 0);
+let tempDate = new Date();
+const tempYear = tempDate.getFullYear();
+const tempMonth = tempDate.getMonth();
+const tempDay = tempDate.getDate();
+
+let futureDate = new Date(tempYear, tempMonth, tempDay + 10, 00, 29, 0);
+
+// let futureDate = new Date(2023, 05, 02, 11, 56, 0);
 // console.log(futureDate);
 const year = futureDate.getFullYear();
 let month = months[futureDate.getMonth()];
